5772cb522b3a4344828e6e8c91e7c4a1422cf924
armandoGutierrez <gutierrezjarr@gmail.com>
2018-09-14 12:41:11 -0800

    Add Character FPS
    Se crea camara en Primera Persona

---
+++ hosp/Assets/Scripts/Camera_FPS.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_FPS : MonoBehaviour {
+
+    public float deltaRotate;
+    public float deltaMovement;
+
+	// Use this for initialization
+	void Start () {
+        deltaRotate = 80f;
+        deltaMovement = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rotate();
+        Movement();
+	}
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0f, deltaRotate, 0f) * Time.deltaTime);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(new Vector3(0f, -deltaRotate, 0f) * Time.deltaTime);
+            }
+        }
+    }
+
+    void Movement()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0f, 0f, -deltaMovement) * Time.deltaTime);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(new Vector3(0f, 0f, deltaMovement) * Time.deltaTime);
+            }
+        }
+    }
+
+}
